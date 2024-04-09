954ee45b9f94730932b3aeeb6655f0d8094d3c2f
MohdAnwaruddin <anwaruddinmohammad19@gmail.com>
2024-04-09 00:42:20 -0400

    login check

--- src/app/components/Login/index.tsx
+++ src/app/components/Login/index.tsx
@@ -64,7 +64,7 @@ const Login = () => {
 
       try {
         const response = await axiosInstance.post(
-          'https://reactserver-pink.vercel.app/login',
+          'https://reactserver-pink.vercel.app/api/auth/login',
           data,
           config
         );
